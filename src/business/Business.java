4df23e8a1b84586387148a3c7761c139e6489030
VicAzevedo <azevedomvictoria@gmail.com>
2018-04-08 21:31:24 -0300

    atributos

--- src/business/Business.java
+++ src/business/Business.java
@@ -2,8 +2,45 @@ package business;
 
 public class Business {
 	
-	public static void main(String args[]) {
-		
+	private String nome;
+	private String cpf;
+	private int idade;
+	private char genero;
+	private int numSocio;
+	
+	public Business (String nome, String cpf, int idade, char genero) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.idade = idade;
+		this.genero = genero;
+		numSocio = 0;		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public char getGenero() {
+		return genero;
+	}	
+	
+	public int getNumSocio() {
+		return numSocio;
+	}
+	
+	public boolean retSocio(int numSocio) {
+		for (Business ){
+			
+			for(T elemento: elementos)
+		}
 	}
 
 }
